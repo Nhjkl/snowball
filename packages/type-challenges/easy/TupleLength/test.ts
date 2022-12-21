2f6147df3e740ba34e356793d3b9f240dae8dd86
liuxian <liuxiangogo@gmail.com>
2022-12-21 17:48:17 +0800

    type-challenges: TupleLength

---
+++ packages/type-challenges/easy/TupleLength/test.ts
@@ -0,0 +1,16 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<'hello world'>,
+]
+
+type tsl1 = { [k in keyof typeof tesla]: (typeof tesla)[k] }
+type tsl2 = (typeof tesla)[number]
--- packages/type-challenges/easy/TupleToObject/TupleToObject.ts
+++ packages/type-challenges/easy/TupleToObject/TupleToObject.ts
@@ -4,6 +4,9 @@ type TupleToObject<T extends readonly ObjKey[]> = {
   [K in T[number]]: K // types 世界里数组循环 T[number]
 }
 
+const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
+type tsl2 = (typeof tesla)[number] // type tsl2 = "tesla" | "model 3" | "model X" | "model Y"
+
 type TestTupleToObject<T extends readonly any[]> = {
   [K in keyof T]: K
 }
