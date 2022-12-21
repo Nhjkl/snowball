cda80611d4bf6a4f385eb6f224c28e1aba750bc1
liuxian <liuxiangogo@gmail.com>
2022-12-21 10:33:26 +0800

    docs: up

--- packages/type-challenges/easy/TupleToObject/TupleToObject.ts
+++ packages/type-challenges/easy/TupleToObject/TupleToObject.ts
@@ -1,3 +1,11 @@
-type TupleToObject<T extends any[]> = {
-  [K in T[number]]: K
+type ObjKey = number | string | symbol
+
+type TupleToObject<T extends readonly ObjKey[]> = {
+  [K in T[number]]: K // types 世界里数组循环 T[number]
 }
+
+type TestTupleToObject<T extends readonly any[]> = {
+  [K in keyof T]: K
+}
+
+type testTuple = TestTupleToObject<['liu', 'xian']> // ['0', '1']
--- packages/type-challenges/easy/TupleToObject/test.ts
+++ packages/type-challenges/easy/TupleToObject/test.ts
@@ -4,12 +4,24 @@ const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const tupleNumber = [1, 2, 3, 4] as const
 const tupleMix = [1, '2', 3, '4'] as const
 
-type tupleType = typeof tuple;
+type typeofTuple = typeof tuple;
+
 
 type cases = [
-  Expect<Equal<TupleToObject<tupleType>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeofTuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
   Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
   Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
 ]
 
+// @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
+
+type tupleType = ['tesla', 'model 3'];
+type keyofTuple = keyof tupleType
+
+let _keyofTuple: keyofTuple;
+
+_keyofTuple = '0'
+_keyofTuple = '1'
+// @ts-expect-error
+_keyofTuple = '2'
