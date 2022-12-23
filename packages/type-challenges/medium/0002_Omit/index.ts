9858910d5fa840116dc1aa6f411ff59c66e31eed
liuxian <liuxiangogo@gmail.com>
2022-12-23 11:44:19 +0800

    类型组合思维

--- packages/type-challenges/medium/0002_Omit/index.ts
+++ packages/type-challenges/medium/0002_Omit/index.ts
@@ -3,10 +3,12 @@ type MyOmit<T, K extends keyof T> = {
   [P in MyExclude<keyof T, K>] : T[P]
 }
 
+type MyOmit1<T, K extends keyof T> = MyPick<T, MyExclude<keyof T, K>>
+
 interface Todo {
   title: string
   description: string
   completed: boolean
 }
 
-type MuOmit_test1 = MyOmit<Todo, 'description'>
+type MuOmit_test1 = MyOmit1<Todo, 'description'>
