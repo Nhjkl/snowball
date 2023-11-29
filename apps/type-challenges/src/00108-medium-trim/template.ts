9ca1983a9dca007f705fba9617c3bfd42034b982
liuxian <liuxiangogo@gmail.com>
2023-11-29 23:01:29 +0800

    feat(type-challenges): trim

---
+++ apps/type-challenges/src/00108-medium-trim/template.ts
@@ -0,0 +1,3 @@
+type _TrimRight<S extends string> = S extends `${infer L}${' ' | '\n' | '\t'}` ? TrimRight<L> : S;
+type _TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer R}` ? TrimLeft<R> : S;
+type Trim<S extends string> = _TrimRight<_TrimLeft<S>>;
---
+++ apps/type-challenges/src/04803-medium-trim-right/template.ts
@@ -0,0 +1,1 @@
+type TrimRight<S extends string> = S extends `${infer L}${' ' | '\n' | '\t'}` ? TrimRight<L> : S;
