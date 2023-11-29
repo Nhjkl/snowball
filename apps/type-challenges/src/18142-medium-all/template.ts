103ba37d647a770be74928c2d0840abbf84ec732
liuxian <liuxiangogo@gmail.com>
2023-11-29 22:52:30 +0800

    feat(type-challenges): type all

---
+++ apps/type-challenges/src/18142-medium-all/template.ts
@@ -0,0 +1,7 @@
+type _Equal<A, B> = (<T>() => T extends A ? true : false) extends <T>() => T extends B ? true : false ? true : false;
+
+type All<Arr extends unknown[], T> = Arr extends [infer F, ...infer R]
+  ? true extends _Equal<F, T>
+    ? All<R, T>
+    : false
+  : true;
